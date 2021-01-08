211f3ebb0722cfc7b1dfb1d0e501d9afae5cd466
孙志强 <1181288394@qq.com>
2021-01-08 23:22:12 +0800

    init-引入全局样式以及重置样式

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-
-
+// 导入 重置样式
+import './assets/styles/reset.css'
+// 导入 全局样式
+import './assets/styles/global.css'
 ReactDOM.render(
 
   <App />, document.getElementById('root')
