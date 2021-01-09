11bc03fc37dff016cc148eb1dc54e4b4876e2dc6
孙志强 <1181288394@qq.com>
2021-01-09 19:00:06 +0800

    首页-左侧菜单渲染完成

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
 
 import { Layout, Menu, Icon, Button, Message } from 'antd'
+import { Link } from 'react-router-dom'
 import './Home.scss'
+
+const { SubMenu } = Menu
 const { Header, Sider, Content } = Layout
 export default class Home extends Component {
-  state = {
-    collapsed: false,
+  constructor(props) {
+    super(props)
+    this.state = {
+      collapsed: false,
+      menuList: [],
+    }
+  }
+
+  componentDidMount() {
+    this.getMenuList()
+  }
+  getMenuList = async () => {
+    const { data: res } = await this.$http.get('menus')
+    if (res.meta.status !== 200) return Message.error(res.meta.msg)
+    this.setState({
+      menuList: res.data,
+    })
+    console.log(this.state.menuList)
   }
 
   toggle = () => {
@@ -22,54 +41,52 @@ export default class Home extends Component {
     // 3.跳到登录页面
     this.props.history.replace('/login')
   }
+
+  // 渲染组件
+  mountedMenuList = () => {
+    return this.state.menuList.map((item) => {
+      return (
+        <SubMenu title={item.authName} key={item.id}>
+          {item.children.map((item) => {
+            return (
+              <Menu.Item
+                title={item.authName}
+                key={item.id}
+                style={{ color: '#fff' }}
+              >
+                <Link to={item.path}>{item.authName}</Link>
+                {item.path}
+              </Menu.Item>
+            )
+          })}
+        </SubMenu>
+      )
+    })
+  }
   render() {
     return (
       <div>
-        <Layout>
+        <Layout className="home-container">
           <div className="home-header">
             <Header className="title">商城后台管理系统</Header>
             <Button className="btn" onClick={this.loginout}>
               退出
             </Button>
           </div>
-        </Layout>
-
-        <Layout className="home-container">
-          {/* 左侧,侧边栏 */}
-          <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-              <Menu.Item key="1">
-                <Icon type="user" />
-                <span>nav 1</span>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <Icon type="video-camera" />
-                <span>nav 2</span>
-              </Menu.Item>
-              <Menu.Item key="3">
-                <Icon type="upload" />
-                <span>nav 3</span>
-              </Menu.Item>
-            </Menu>
-          </Sider>
           <Layout>
-            <Header style={{ background: '#fff', padding: 0 }}>
-              <Icon
-                className="trigger"
-                type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-                onClick={this.toggle}
-              />
-            </Header>
-            <Content
-              style={{
-                margin: '24px 16px',
-                padding: 24,
-                background: '#fff',
-                minHeight: 280,
-              }}
-            >
-              Content
-            </Content>
+            <Sider width={200} style={{ background: '#fff' }}>
+              <Menu
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                style={{ height: '100%', borderRight: 0 }}
+              >
+                {this.mountedMenuList()}
+              </Menu>
+            </Sider>
+            <Layout style={{ padding: '0 24px 24px' }}>
+              <Content></Content>
+            </Layout>
           </Layout>
         </Layout>
       </div>
